7bcec844983acffa8523bd94a02f47775e47c07e
Chris Fleming <me@chrisfleming.org>
2018-02-03 09:55:15 +0000

    generated to git

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/chrisfleming-org-built/"
+http_images_path = "/chrisfleming-org-built/images"
+http_fonts_path = "/chrisfleming-org-built/fonts"
+css_dir = "public/chrisfleming-org-built/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
